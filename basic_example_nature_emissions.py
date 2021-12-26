7d56a8a168b5e4cc1252b0bbe010932a323d00fc
Bo Fu <42598161+pkufubo@users.noreply.github.com>
2021-12-26 18:26:28 +0800

    tiny change in script

--- basic_example_nature_emissions.py
+++ basic_example_nature_emissions.py
@@ -21,18 +21,18 @@ nMC = 500
 ## note: requires defining a 'mod_region' and the number of MC elements 'nMC'
 RUN_a = run_model(OSCAR, (1750, 1750, 2014), mod_region='AR6_22reg', nMC=nMC, output=True,
                   nature_aerosols=True)
-OUT = RUN_a['Out_hist']
+OUT_a = RUN_a['Out_hist']
 ## plot global temperature change
-plt.plot(OUT.year, OUT.D_Tg.mean('config'), color='b', lw=2)
-plt.fill_between(OUT.year, OUT.D_Tg.mean('config') - OUT.D_Tg.std('config'), OUT.D_Tg.mean('config') + OUT.D_Tg.std('config'), color='b', alpha=0.5)
+plt.plot(OUT_a.year, OUT_a.D_Tg.mean('config'), color='b', lw=2)
+plt.fill_between(OUT_a.year, OUT_a.D_Tg.mean('config') - OUT_a.D_Tg.std('config'), OUT_a.D_Tg.mean('config') + OUT_a.D_Tg.std('config'), color='b', alpha=0.5)
 
 # same as above, but without nature emission module
 
-RUN_a = run_model(OSCAR, (1750, 1750, 2014), mod_region='AR6_22reg', nMC=nMC, output=True,
+RUN_b = run_model(OSCAR, (1750, 1750, 2014), mod_region='AR6_22reg', nMC=nMC, output=True,
                   nature_aerosols=False)
-OUT = RUN_a['Out_hist']
+OUT_b = RUN_b['Out_hist']
 ## plot global temperature change
-plt.plot(OUT.year, OUT.D_Tg.mean('config'), color='k', lw=2)
-plt.fill_between(OUT.year, OUT.D_Tg.mean('config') - OUT.D_Tg.std('config'), OUT.D_Tg.mean('config') + OUT.D_Tg.std('config'), color='k', alpha=0.5)
+plt.plot(OUT_b.year, OUT_b.D_Tg.mean('config'), color='k', lw=2)
+plt.fill_between(OUT_b.year, OUT_b.D_Tg.mean('config') - OUT_b.D_Tg.std('config'), OUT_b.D_Tg.mean('config') + OUT_b.D_Tg.std('config'), color='k', alpha=0.5)
 
 
